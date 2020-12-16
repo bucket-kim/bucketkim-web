e4029ad7d111a16eb60ef81a7d943d4639add608
Brian Kim <brian.kyounghoon.kim@gmail.com>
2020-12-16 20:02:40 +0900

    JS menu actions

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// handle scrolling when menu onclick happens 
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+  console.log(e.target.dataset.link);
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+})
